dd7d6699754bd72f9c6ac9acbf106565e31d7b0d
zackjackson <zackary.l.jackson@gmail.com>
2019-03-27 21:57:54 -0700

    fix: transforming all types of functions

--- manual/source.js
+++ manual/source.js
@@ -3,10 +3,13 @@ import SomeComponent from './someDir/SomeComponent';
 
 const Test = (props, state, actions) => <div>{state.title}</div>;
 
-const TestTwo = (props, state, actions) => <div>{state.title}</div>;
+const TestTwo = (props, state, actions) => {
+  console.log('peace redux')
+  return (<div>{state.title}</div>)
+};
 
 
-const TestThree = () => (
+const TestThree = (props, state, actions) => (
   <button onClick={() => dispatch({
     type: 'fake',
     payload: 'nothin',
@@ -14,6 +17,14 @@ const TestThree = () => (
   >test
   </button>
 );
+
+function TestFour(props, state, actions) {
+  return (<div>testin</div>)
+}
+
+const TestFive = function(props, state, actions) {
+  return (<div>testn</div>)
+}
 
 
 export { Test, TestTwo, TestThree };
