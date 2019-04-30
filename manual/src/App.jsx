20e21ef5e60627939c95fd826f164f4194a5ad7a
zackjackson <zackary.l.jackson@gmail.com>
2019-04-29 21:04:28 -0700

    added a working example given the new refactors

--- manual/src/App.jsx
+++ manual/src/App.jsx
@@ -1,12 +1,15 @@
-import React, { Component } from 'react';
-import { hot } from 'react-hot-loader';
+import React, {Component} from 'react';
+import {hot} from 'react-hot-loader';
 import HelloWorld from './components/hello-world';
 
-import('http://localhost:8080/importManifest.js')
-import(/* importUrl */'http://localhost:8080/' + window.entryManifest['demo-build']['other.js']).then(({ fakemodule }) => {
-  console.log(fakemodule);
-  fakemodule.default();
-});
+import('http://localhost:8080/importManifest.js').then(() => {
+  import(/* importUrl */'http://localhost:8080/' + window.entryManifest['demo-build']['other.js']).then(({externalFunction}) => {
+    console.log('Webpack Modules:',__webpack_modules__);
+    console.log('Require Statement:',__webpack_require__('externalFunction'))
+    __webpack_require__('externalFunction').default()
+  });
+})
+
 
 class App extends Component {
   constructor(props) {
