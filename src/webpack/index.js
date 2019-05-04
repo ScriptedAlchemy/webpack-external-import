362f312d3766963f23e48455ad3c9dcf380657a7
zackjackson <zackary.l.jackson@gmail.com>
2019-05-03 21:28:13 -0700

    adding to babel outputs to gitignore

--- src/webpack/index.js
+++ src/webpack/index.js
@@ -237,20 +237,6 @@ class URLImportPlugin {
 
                 }
               }
-              // if (module.id === null && module.libIdent) {
-              //   const id = module.libIdent({
-              //     context: this.options.context || compiler.options.context
-              //   });
-              //   const hash = createHash(options.hashFunction);
-              //   hash.update(id);
-              //   const hashId = hash.digest(options.hashDigest);
-              //   let len = options.hashDigestLength;
-              //   while (usedIds.has(hashId.substr(0, len))) len++;
-              //
-              //   module.id = hashId.substr(0, len);
-              //   module._source._value = module._source._value.replace('__respond_pending_chunk_id__', module.id)
-              //   usedIds.add(module.id);
-              // }
             }
           }
         );
