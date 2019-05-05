b58150be7554fb289e597f616bcbc7e9531f0af0
zackjackson <zackary.l.jackson@gmail.com>
2019-05-05 14:48:59 -0700

    Adding Jest snapshots for babel plugin

---
+++ test/babel/cases/mixed-expression-url/index.js
@@ -0,0 +1,7 @@
+import(/* importUrl */'http://localhost:3002/' + window.entryManifest['website-two']['hello-world.js']);
+
+const someUrl = 'http://something.com';
+
+import(/*importUrl */someUrl)
+
+// import(/*importUrl */ ()=> 'http://thing.com/js/index.js')
---
+++ test/babel/cases/non-module-extracting-promise/index.js
@@ -0,0 +1,3 @@
+import('http://localhost:3002/importManifest.js').then(() => {
+  console.log('this has been loaded')
+})
