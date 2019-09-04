b435733006903b0d5def61d4e00246b160582a60
ScriptedAlchemy <zjackson@lululemon.com>
2019-09-04 11:13:52 -0700

    stashing changes

--- manual/webpack/webpack.prod.js
+++ manual/webpack/webpack.prod.js
@@ -28,17 +28,12 @@ module.exports = (commonPaths) => ({
     // https://twitter.com/wSokra/status/969633336732905474
     // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
     splitChunks: {
+      chunks: 'all',
       cacheGroups: {
         vendors: {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendors',
           chunks: 'initial',
-        },
-        async: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'async',
-          chunks: 'async',
-          minChunks: 4,
         },
       },
     },
