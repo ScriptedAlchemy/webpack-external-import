32a85f94004eda046d2dcee755ba77fff8c2b995
ScriptedAlchemy <zjackson@lululemon.com>
2019-09-18 17:30:56 -0700

    hardening against errors

--- src/corsImport.js
+++ src/corsImport.js
@@ -4,6 +4,12 @@ const corsImport = (url) => {
   if (!url) {
     return new Promise((resolve, reject) => reject('no url in corsImport'));
   }
+  if (loadjs.isDefined(url)) {
+    return new Promise((resolve, reject) => {
+      resolve();
+    });
+  }
+
   loadjs(url, url);
 
   return new Promise((resolve, reject) => {
