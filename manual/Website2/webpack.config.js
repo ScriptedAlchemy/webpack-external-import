a5b3d671a5d992a5ff85bff8754b01b3ca188c09
zackjackson <zackary.l.jackson@gmail.com>
2020-01-01 11:34:57 -0800

    working on internalization

--- manual/Website2/webpack.config.js
+++ manual/Website2/webpack.config.js
@@ -11,3 +11,5 @@ module.exports = configFactory(siteId, {
     }),
   ],
 });
+
+console.log(module.exports)
--- manual/webpack/webpack.prod.js
+++ manual/webpack/webpack.prod.js
@@ -1,16 +1,16 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const WriteFilePlugin = require('write-file-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = commonPaths => ({
-  mode: 'production',
+  mode: "production",
   entry: commonPaths.entry,
   output: {
     filename: `${commonPaths.jsFolder}/[name].[hash].js`,
     path: commonPaths.outputPath,
-    chunkFilename: `${commonPaths.jsFolder}/[name].[chunkhash].js`,
+    chunkFilename: `${commonPaths.jsFolder}/[name].[chunkhash].js`
   },
   optimization: {
     minimizer: [
@@ -20,26 +20,26 @@ module.exports = commonPaths => ({
         parallel: true,
         // Enable file caching
         cache: true,
-        sourceMap: true,
+        sourceMap: true
       }),
-      new OptimizeCSSAssetsPlugin(),
+      new OptimizeCSSAssetsPlugin()
     ],
     // Automatically split vendor and commons
     // https://twitter.com/wSokra/status/969633336732905474
     // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
       cacheGroups: {
         vendors: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'initial',
-        },
-      },
+          name: "vendors",
+          chunks: "initial"
+        }
+      }
     },
     // Keep the runtime chunk seperated to enable long term caching
     // https://twitter.com/wSokra/status/969679223278505985
-    runtimeChunk: true,
+    runtimeChunk: true
   },
 
   module: {
@@ -49,18 +49,18 @@ module.exports = commonPaths => ({
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               sourceMap: false,
               modules: true,
               camelCase: true,
-              localIdentName: '[local]___[hash:base64:5]',
-            },
+              localIdentName: "[local]___[hash:base64:5]"
+            }
           },
-          'sass-loader',
-        ],
-      },
-    ],
+          "sass-loader"
+        ]
+      }
+    ]
   },
   devServer: {
     contentBase: commonPaths.outputPath,
@@ -68,17 +68,17 @@ module.exports = commonPaths => ({
     hot: true,
     port: `300${commonPaths.siteId}`,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*',
-    },
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*"
+    }
   },
   plugins: [
     new WriteFilePlugin(),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: `${commonPaths.cssFolder}/[name].css`,
-      chunkFilename: `${commonPaths.cssFolder}/[name].css`,
-    }),
+      chunkFilename: `${commonPaths.cssFolder}/[name].css`
+    })
   ],
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map"
 });
