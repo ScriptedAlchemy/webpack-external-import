dcbfd87de45378c4afae147cd44209ccf2418402
zackjackson <zackary.l.jackson@gmail.com>
2020-01-22 15:40:05 -0800

    adding website 3

---
+++ manual/Website3/src/components/Title/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "react-select";
+import "./style.css";
+
+export const Title = ({ title }) => {
+  window.wasExternalFunctionCalled = true;
+  return (
+    <h1 className="with-css">
+      TITLE COMPONENT:
+      {title}
+    </h1>
+  );
+};
+
+/* externalize:TitleComponentWithCSSFile */
