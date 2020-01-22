c2d7e1228ea8230b7bb15002bcea520c3dc43d82
zackjackson <zackary.l.jackson@gmail.com>
2020-01-22 15:40:05 -0800

    adding website 3

---
+++ manual/packages/Website3/src/components/Title/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "react-select";
+import "./style.css";
+
+export const Title = ({ title }) => {
+  window.wasExternalFunctionCalled = true;
+  console.log("TitleComponent interleaving successful");
+  return (
+    <h1>
+      TITLE COMPONENT:
+      {title}
+    </h1>
+  );
+};
+
+/* externalize:TitleComponent */
