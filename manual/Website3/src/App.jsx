96cad30272aa3b693a39aafac0c3779cfe2b798f
zackjackson <zackary.l.jackson@gmail.com>
2020-01-22 15:31:25 -0800

    adding website 3

---
+++ manual/Website3/src/App.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+import {Title} from './components/Title';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      component: null,
+    };
+  }
+
+  componentDidMount() {
+    import('./components/hello-world').then((HelloWorld) => {
+      this.setState({ component: HelloWorld.Test });
+    });
+  }
+
+  render() {
+    if (!this.state.component) return null;
+    const { component: HelloWorld } = this.state;
+    return <Title title="Hello, from Website3" />;
+  }
+}
+
+export default hot(module)(App);
