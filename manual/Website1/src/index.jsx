d8f6d9df678eb40f2c2e5034367b823507bc5b92
Joshua Kaminetsky <ethersage@users.noreply.github.com>
2020-02-02 18:10:52 -0700

    chore(logging): Remove console.log statements from production build (#110)
    Co-authored-by: kami <jmkaminetsky@gmail.com>

--- manual/Website1/src/index.jsx
+++ manual/Website1/src/index.jsx
@@ -7,6 +7,10 @@ import { corsImport } from "../../../index";
 Promise.all([
   corsImport(`http://localhost:3002/importManifest.js?${Date.now()}`),
   corsImport(`http://localhost:3003/importManifest.js?${Date.now()}`)
-]).then(() => {
-  ReactDOM.render(<App />, document.getElementById("app"));
-});
+])
+  .then(() => {
+    ReactDOM.render(<App />, document.getElementById("app"));
+  })
+  .catch(err => {
+    console.log(err);
+  });
