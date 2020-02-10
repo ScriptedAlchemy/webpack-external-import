85bb89792f106da9676ff1d3ae8a2bbda21afb2b
zackjackson <zackary.l.jackson@gmail.com>
2020-02-09 17:16:52 -0800

    chore: updating demos
    removing yarn links and adding externals configs for testing

--- manual/website2/webpack.config.js
+++ manual/website2/webpack.config.js
@@ -1,11 +1,19 @@
 const webpack = require("webpack");
 const configFactory = require("../webpack/webpackConfigFactory");
+const URLImportPlugin = require("../../webpack");
 
 const siteId = 2;
 module.exports = configFactory(siteId, {
-  plugins: [],
-  externals: {
-    // react: "React"
-  }
+  plugins: [
+    new URLImportPlugin({
+      manifestName: "website-2",
+      fileName: "importManifest.js",
+      basePath: ``,
+      publicPath: `//localhost:3002/`,
+      transformExtensions: /^(gz|map)$/i,
+      useExternals: {
+        react: "React"
+      }
+    })
+  ]
 });
-
