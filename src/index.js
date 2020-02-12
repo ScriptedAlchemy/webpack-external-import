1029f0ecef165f8d279a600e21e914693f322bb8
Marais Rossouw <me@maraisr.com>
2020-02-12 11:49:25 +1000

    feat: Init the future of v3 :rocket:

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import corsImport from "./corsImport";
 import ExternalComponent from "./react";
 
-export { corsImport, ExternalComponent };
+import ContainerPlugin from "./ContainerPlugin";
+import ContainerReferencePlugin from "./ContainerReferencePlugin";
+
+export {
+  corsImport,
+  ExternalComponent,
+  ContainerPlugin,
+  ContainerReferencePlugin
+};
