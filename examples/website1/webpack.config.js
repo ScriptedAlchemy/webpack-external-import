12746d74ee9cccbb723dfde518581536fd44a71e
Marais Rossouw <me@maraisr.com>
2020-02-18 12:03:57 +1000

    chore: New project structure

---
+++ examples/website1/webpack.config.js
@@ -0,0 +1,44 @@
+const { ContainerReferencePlugin } = require('webpack-external-import');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index',
+	cache: false,
+
+	mode: 'production',
+
+	optimization: {
+		minimize: false,
+	},
+
+	output: {
+		publicPath: 'http://localhost:3001/',
+	},
+
+	resolve: {
+		extensions: ['.jsx', '.js', '.json'],
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				loader: require.resolve('babel-loader'),
+				options: {
+					rootMode: 'upward',
+					presets: [require.resolve('@babel/preset-react')],
+				},
+			},
+		],
+	},
+
+	plugins: [
+		new ContainerReferencePlugin({
+			remoteType: 'global',
+			remotes: ['website2'],
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/template.html',
+		}),
+	],
+};
---
+++ packages/plugins/ContainerReferencePlugin.js
@@ -0,0 +1,141 @@
+const RemoteModule = require('./RemoteModule');
+
+const UNSPECIFIED_EXTERNAL_TYPE_REGEXP = /^[a-z0-9]+ /;
+const PLUGIN_NAME = 'ContainerReferencePlugin';
+
+class RemoteModuleFactoryPlugin {
+	constructor(type, remotes) {
+		this.remoteType = type;
+		this.remotes = remotes;
+	}
+
+	apply(normalModuleFactory) {
+		const globalType = this.remoteType;
+		normalModuleFactory.hooks.factorize.tapAsync(
+			'RemoteModuleFactoryPlugin',
+			(data, callback) => {
+				const { context } = data;
+				const dependency = data.dependencies[0];
+
+				const handleRemote = (value, type, callback) => {
+					if (value === false) {
+						// Not externals, fallback to original factory
+						return callback();
+					}
+					/** @type {string} */
+					let externalConfig;
+					if (value === true) {
+						externalConfig = dependency.request;
+					} else {
+						externalConfig = value;
+					}
+					// When no explicit type is specified, extract it from the externalConfig
+					if (
+						type === undefined &&
+						UNSPECIFIED_EXTERNAL_TYPE_REGEXP.test(externalConfig)
+					) {
+						const idx = externalConfig.indexOf(' ');
+						type = externalConfig.substr(0, idx);
+						externalConfig = externalConfig.substr(idx + 1);
+					}
+
+					callback(
+						null,
+						new RemoteModule(
+							externalConfig,
+							type || globalType,
+							dependency.request,
+						),
+					);
+				};
+
+				const handleExternals = (remotes, callback) => {
+					if (typeof remotes === 'string') {
+						const requestScope = data?.request
+							?.split('/')
+							?.shift?.();
+
+						if (this.remotes?.includes(requestScope)) {
+							return handleRemote(
+								dependency.request,
+								undefined,
+								callback,
+							);
+						}
+					} else if (Array.isArray(remotes)) {
+						let i = 0;
+						const next = () => {
+							let asyncFlag;
+							const handleExternalsAndCallback = (
+								err,
+								module,
+							) => {
+								if (err) return callback(err);
+								if (!module) {
+									if (asyncFlag) {
+										asyncFlag = false;
+										return;
+									}
+									return next();
+								}
+								callback(null, module);
+							};
+
+							do {
+								asyncFlag = true;
+								if (i >= remotes.length) return callback();
+								handleExternals(
+									remotes[i++],
+									handleExternalsAndCallback,
+								);
+							} while (!asyncFlag);
+							asyncFlag = false;
+						};
+
+						next();
+						return;
+					} else if (
+						typeof remotes === 'object' &&
+						Object.prototype.hasOwnProperty.call(
+							remotes,
+							dependency.request,
+						)
+					) {
+						return handleRemote(
+							remotes[dependency.request],
+							undefined,
+							callback,
+						);
+					}
+					callback();
+				};
+
+				handleExternals(this.remotes, callback);
+			},
+		);
+	}
+}
+
+export default class ContainerReferencePlugin {
+	constructor(options) {
+		console.clear();
+
+		this.options = {
+			remoteType: options.remoteType ?? null, // TODO: Mark this as required?
+			remotes: options.remotes ?? [],
+			override: options.override ?? {},
+		};
+
+		// TODO: Apply some validation around what was passed in.
+	}
+
+	apply(compiler) {
+		const { remotes, remoteType } = this.options;
+
+		compiler.hooks.compile.tap(PLUGIN_NAME, ({ normalModuleFactory }) => {
+			new RemoteModuleFactoryPlugin(remoteType, remotes).apply(
+				normalModuleFactory,
+			);
+		});
+	}
+}
