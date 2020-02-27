25373bd720ea689a1e9a09f6f6c0dee93b6542fa
ScriptedAlchemy <zjackson@lululemon.com>
2020-02-26 16:17:25 -0800

    working!

--- examples/website1/src/HelloWorld.jsx
+++ examples/website1/src/HelloWorld.jsx
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense, useState } from 'react';
 
-const Title = lazy(() => import('website2/Title').then(mod => mod));
+const Title = lazy(() => import('website2/Title'));
 
 export default () => {
 	return (
